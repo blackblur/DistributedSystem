7dfee1f0483023e17716f16765c582ce4e15acd4
Lennart Dopatka <lennartdopatka@gmail.de>
2017-12-30 16:16:12 +0100

    initial commit

---
+++ src/main/java/com/disy/disy/DisyApplication.java
@@ -0,0 +1,12 @@
+package com.disy.disy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisyApplication.class, args);
+	}
+}
